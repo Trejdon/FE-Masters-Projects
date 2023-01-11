bfec4a1c399e321469e5010a9e22e85eaf941155
Levi Trejo <ltrejo32@gmail.com>
2023-01-10 18:32:14 -0600

    feat(Game): Jerksdle v1 complete

--- IntroWebDev3/wordGameClone/game.js
+++ IntroWebDev3/wordGameClone/game.js
@@ -13,22 +13,84 @@ const isLetter = (character) => {
     return /^[a-zA-Z]$/.test(character)
 }
 
+const setLoading = (isLoading) => {
+    const loader = document.querySelector(".loader");
+    (isLoading) ? loader.classList.remove("hidden") : loader.classList.add("hidden")
+}
+
 const gatherCurrentWord = () => {
     let word = "";
     for (let i = 1; i < 6; i++){
         word += document.getElementById(`${currentGuess}-${i}`).innerText;
-        console.log({ word })
     }
     currentWord = word;
 }
 
 async function getSecretWord() {
     const response = await fetch(SECRETURL);
-    const responObj = await response.json();
-    secretWord = responObj.word;
+    const responseObj = await response.json();
+
+    secretWord = responseObj.word.toUpperCase();
 }
 
-const handleKeyPress = (event) => {
+const createMap = (wordArray) => {
+    const characterCounts = {};
+    for (let i = 0; i < wordArray.length; i++) {
+        const character = wordArray[i];
+        if (characterCounts[character] > 0) {
+            characterCounts[character]++
+        } else {
+            characterCounts[character] = 1;
+        }
+    }
+
+    return result;
+}
+
+const evaluateCharacter = (character, idx) => {
+    if (secretWord.charAt(idx) === character) {
+        return "correct";
+    } else if (secretWord.includes(character)) {
+        return "present";
+    } else {
+        return "incorrect";
+    }
+}
+
+const evaluateWord = (word) => {
+    const result = [];
+    const wordArray = word.split('')
+
+    wordArray.map((letter, idx) => {
+        const eval = evaluateCharacter(letter, idx);
+        result.push(eval)
+    })
+
+    return result;
+}
+
+const isCorrect = (resultArr) => {
+    const correctArr = resultArr.filter(result => result === "correct")
+    return correctArr.length === 5;
+}
+
+const updateGuessClasses = (resultArr) => {
+    for (let i = 0; i < resultArr.length; i++) {
+        if (resultArr[i] !== "incorrect") {
+            document.getElementById(`${currentGuess}-${i+1}`).classList.add(resultArr[i])
+        }
+    }
+}
+
+const validateCurrentWord = async (word) => {
+    setLoading(true);
+    const response = await fetch(VALIDATE_URL, { "method": "POST", "body": JSON.stringify({ word })});
+    const responseObj = await response.json();
+    setLoading(false);
+    return responseObj.validWord;;
+}
+
+const handleKeyPress = async (event) => {
     if (isLetter(event.key)) {
         // set the current tile text
         currentTile.innerText = event.key
@@ -53,18 +115,39 @@ const handleKeyPress = (event) => {
         if (currentLetter === 5 && currentTile.innerText !== "") {
             // Gather the word from the current guess
             gatherCurrentWord();
+
+            // Set loading to indicate the guess is being validated
+
             // Validate the word
+            const isValid = await validateCurrentWord(currentWord);
 
+            if(isValid) {
+                console.log("Word is valid")
+            } else {
+                console.log("Invalid word")
+            };
             // Evaluate the guess word against the secret word
-
-            // If correct, update the CSS classes of the current guess and alert on win condition
-
+            const result = evaluateWord(currentWord);
+            // Update the CSS classes of the current guess
+            updateGuessClasses(result);
+            // Win, lose, or guess again
+            if(isCorrect(result)){
+                alert("You Win")
+            } else if (currentGuess === 6){
+                alert(`What a Loser.  The word was ${secretWord}!`)
+            } else {
+                currentGuess += 1;
+                currentLetter = 1;
+                currentWord = "";
+                currentTile = document.getElementById(`${currentGuess}-${currentLetter}`)
+            }
             // If incorrect, update the CSS classes of the current guess, setup the app for next guess if current guess is < 6
         }
     }
 }
 
 const init = () => {
+    setLoading(true);
     getSecretWord();
     document.querySelector("body").addEventListener("keydown", (event) => {
         handleKeyPress(event)
@@ -72,5 +155,3 @@ const init = () => {
 }
 
 init()
-
-console.log({ secretWord })
