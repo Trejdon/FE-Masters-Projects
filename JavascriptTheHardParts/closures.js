079f97c562a47714ec3f7789ee1af1913199a9ed
Levi Trejo <ltrejo32@gmail.com>
2023-01-18 14:18:09 -0600

    chore(wip): wip commit for safety

--- JavascriptTheHardParts/closures.js
+++ JavascriptTheHardParts/closures.js
@@ -1,33 +1,37 @@
 // CHALLENGE 1
 function createFunction() {
-
+    return function createdFunction () {
+        console.log("hello");
+    }
 }
 
 // /*** Uncomment these to check your work! ***/
-// const function1 = createFunction();
-// function1(); // => should console.log('hello');
+const function1 = createFunction();
+function1(); // => should console.log('hello');
 
 
 // CHALLENGE 2
 function createFunctionPrinter(input) {
-
+    return function printer () {
+        console.log(input)
+    };
 }
 
 // /*** Uncomment these to check your work! ***/
-// const printSample = createFunctionPrinter('sample');
-// printSample(); // => should console.log('sample');
-// const printHello = createFunctionPrinter('hello');
-// printHello(); // => should console.log('hello');
+const printSample = createFunctionPrinter('sample');
+printSample(); // => should console.log('sample');
+const printHello = createFunctionPrinter('hello');
+printHello(); // => should console.log('hello');
 
 
 // CHALLENGE 3
 function outer() {
-  let counter = 0; // this variable is outside incrementCounter's scope
-  function incrementCounter () {
-    counter ++;
-    console.log('counter', counter);
-  }
-  return incrementCounter;
+    let counter = 0; // this variable is outside incrementCounter's scope
+    function incrementCounter () {
+        counter ++;
+        console.log('counter', counter);
+    }
+    return incrementCounter;
 }
 
 const willCounter = outer();
@@ -37,162 +41,264 @@ const jasCounter = outer();
 // Before your do, guess what will be logged from each function call.
 
 // /*** Uncomment these to check your work! ***/
-// willCounter();
-// willCounter();
-// willCounter();
+willCounter();
+willCounter();
+willCounter();
 
-// jasCounter();
-// willCounter();
+jasCounter();
+willCounter();
 
 
 function addByX(x) {
-
+    return function (num) {
+        console.log(num + x);
+    }
 }
 
 // /*** Uncomment these to check your work! ***/
-// const addByTwo = addByX(2);
-// addByTwo(1); // => should return 3
-// addByTwo(2); // => should return 4
-// addByTwo(3); // => should return 5
+const addByTwo = addByX(2);
+addByTwo(1); // => should return 3
+addByTwo(2); // => should return 4
+addByTwo(3); // => should return 5
 
-// const addByThree = addByX(3);
-// addByThree(1); // => should return 4
-// addByThree(2); // => should return 5
+const addByThree = addByX(3);
+addByThree(1); // => should return 4
+addByThree(2); // => should return 5
 
-// const addByFour = addByX(4);
-// addByFour(4); // => should return 8
-// addByFour(5); // => should return 9
+const addByFour = addByX(4);
+addByFour(4); // => should return 8
+addByFour(5); // => should return 9
 
 
 // CHALLENGE 4
 function once(func) {
-
+    let counter = 0;
+    let output;
+    const inner = function(input) {
+        if (counter === 0) {
+            output = func(input)
+            counter++;
+            return output;
+        } else {
+            counter++;
+            return output;
+        }
+    }
+    return inner;
 }
 
 // /*** Uncomment these to check your work! ***/
-// const onceFunc = once(addByTwo);
-// console.log(onceFunc(4));  // => should log 6
-// console.log(onceFunc(10));  // => should log 6
-// console.log(onceFunc(9001));  // => should log 6
+const onceFunc = once(addByTwo);
+console.log(onceFunc(4));  // => should log 6
+console.log(onceFunc(10));  // => should log 6
+console.log(onceFunc(9001));  // => should log 6
 
 
 // CHALLENGE 5
 function after(count, func) {
+    let totalRuns = 1;
 
+    const runner = function () {
+        if (totalRuns === count) {
+            func();
+        }
+
+        totalRuns++;
+        return;
+    }
+
+    return runner;
 }
 
 // /*** Uncomment these to check your work! ***/
-// const called = function() { console.log('hello') };
-// const afterCalled = after(3, called);
-// afterCalled(); // => nothing is printed
-// afterCalled(); // => nothing is printed
-// afterCalled(); // => 'hello' is printed
+const called = function() { console.log('hello') };
+const afterCalled = after(3, called);
+afterCalled(); // => nothing is printed
+afterCalled(); // => nothing is printed
+afterCalled(); // => 'hello' is printed
 
 
 // CHALLENGE 6
-function delay(func, wait) {
-
-}
+function delay(func, wait, ...args) {
+        setTimeout(func(...args), wait);
+    }
 
 
 // CHALLENGE 7
 function rollCall(names) {
+    let totalRuns = 1;
+    const announce = () => {
+        if (!(totalRuns > names.length)) {
+            console.log(names[totalRuns-1])
+        } else {
+            console.log("Everyone accounted for")
+        }
+        totalRuns++;
+    }
 
+    return announce;
 }
 
 // /*** Uncomment these to check your work! ***/
-// const rollCaller = rollCall(['Victoria', 'Juan', 'Ruth'])
-// rollCaller() // => should log 'Victoria'
-// rollCaller() // => should log 'Juan'
-// rollCaller() // => should log 'Ruth'
-// rollCaller() // => should log 'Everyone accounted for'
+const rollCaller = rollCall(['Victoria', 'Juan', 'Ruth'])
+rollCaller() // => should log 'Victoria'
+rollCaller() // => should log 'Juan'
+rollCaller() // => should log 'Ruth'
+rollCaller() // => should log 'Everyone accounted for'
 
 
 // CHALLENGE 8
 function saveOutput(func, magicWord) {
+    const outputHistory = {}
 
+    const getOutput = (value) => {
+        if (value === magicWord) {
+            console.log({ outputHistory })
+        } else {
+            const output = func(value)
+            outputHistory[value] = output;
+            console.log({output})
+        }
+    }
+
+    return getOutput;
 }
 
 // /*** Uncomment these to check your work! ***/
-// const multiplyBy2 = function(num) { return num * 2; };
-// const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
-// console.log(multBy2AndLog(2)); // => should log 4
-// console.log(multBy2AndLog(9)); // => should log 18
-// console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
+const multiplyBy2 = function(num) { return num * 2; };
+const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
+console.log(multBy2AndLog(2)); // => should log 4
+console.log(multBy2AndLog(9)); // => should log 18
+console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
 
 
 // CHALLENGE 9
 function cycleIterator(array) {
+    let cycleCount = 0;
 
+    const iterator = () => {
+        const iteration = cycleCount % array.length;
+        console.log({output: array[iteration]})
+        cycleCount++
+    }
+
+    return iterator;
 }
 
 // /*** Uncomment these to check your work! ***/
-// const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
-// const getDay = cycleIterator(threeDayWeekend);
-// console.log(getDay()); // => should log 'Fri'
-// console.log(getDay()); // => should log 'Sat'
-// console.log(getDay()); // => should log 'Sun'
-// console.log(getDay()); // => should log 'Fri'
-
+const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
+const getDay = cycleIterator(threeDayWeekend);
+console.log(getDay()); // => should log 'Fri'
+console.log(getDay()); // => should log 'Sat'
+console.log(getDay()); // => should log 'Sun'
+console.log(getDay()); // => should log 'Fri'
 
 // CHALLENGE 10
 function defineFirstArg(func, arg) {
+    const args = [arg];
 
+    const inner = (arg) => {
+        if (!(args[0] === arg)) {
+            args.push(arg)
+            console.log({output: func(...args)})
+        }
+    }
+
+    return inner;
 }
 
 // /*** Uncomment these to check your work! ***/
-// const subtract = function(big, small) { return big - small; };
-// const subFrom20 = defineFirstArg(subtract, 20);
-// console.log(subFrom20(5)); // => should log 15
+const subtract = function(big, small) { return big - small; };
+const subFrom20 = defineFirstArg(subtract, 20);
+console.log(subFrom20(5)); // => should log 15
 
 
 // CHALLENGE 11
 function dateStamp(func) {
+    const stampedObj = {}
+    const stamper = (value) => {
+        stampedObj.date = new Date().toDateString()
+        stampedObj.output = func(value)
+        return stampedObj;
+    }
+
+    return stamper;
 
 }
 
 // /*** Uncomment these to check your work! ***/
-// const stampedMultBy2 = dateStamp(n => n * 2);
-// console.log(stampedMultBy2(4)); // => should log { date: (today's date), output: 8 }
-// console.log(stampedMultBy2(6)); // => should log { date: (today's date), output: 12 }
+const stampedMultBy2 = dateStamp(n => n * 2);
+console.log(stampedMultBy2(4)); // => should log { date: (today's date), output: 8 }
+console.log(stampedMultBy2(6)); // => should log { date: (today's date), output: 12 }
 
 
 // CHALLENGE 12
 function censor() {
+    const censoredPairs = [];
 
+    const cernsorship = (...args) => {
+        const isSingle = args.length === 1;
+        let output = args[0]
+
+        if(!isSingle) {
+            censoredPairs.push(args)
+        } else {
+            for(const pair of censoredPairs) {
+                output = output.replace(pair[0], pair[1])
+            }
+
+            return output;
+        }
+    }
+
+    return cernsorship;
 }
 
 // /*** Uncomment these to check your work! ***/
-// const changeScene = censor();
-// changeScene('dogs', 'cats');
-// changeScene('quick', 'slow');
-// console.log(changeScene('The quick, brown fox jumps over the lazy dogs.')); // => should log 'The slow, brown fox jumps over the lazy cats.'
+const changeScene = censor();
+changeScene('dogs', 'cats');
+changeScene('quick', 'slow');
+console.log(changeScene('The quick, brown fox jumps over the lazy dogs.')); // => should log 'The slow, brown fox jumps over the lazy cats.'
 
 
 // CHALLENGE 13
 function createSecretHolder(secret) {
+    const returnObj = {
+        getSecret() { return secret},
+        setSecret(value) { secret = value}
+    }
+
+    return returnObj;
 
 }
 
 // /*** Uncomment these to check your work! ***/
-// obj = createSecretHolder(5)
-// obj.getSecret() // => returns 5
-// obj.setSecret(2)
-// obj.getSecret() // => returns 2
+obj = createSecretHolder(5)
+console.log(obj.getSecret()) // => returns 5
+console.log(obj.setSecret(2))
+console.log(obj.getSecret()) // => returns 2
+
 
 
 // CHALLENGE 14
 function callTimes() {
+    let timesRun = 0;
 
+    const runnings = () => {
+        timesRun++;
+        return timesRun
+    }
+
+    return runnings;
 }
 
 // /*** Uncomment these to check your work! ***/
-// let myNewFunc1 = callTimes();
-// let myNewFunc2 = callTimes();
-// myNewFunc1(); // => 1
-// myNewFunc1(); // => 2
-// myNewFunc2(); // => 1
-// myNewFunc2(); // => 2
+let myNewFunc1 = callTimes();
+let myNewFunc2 = callTimes();
+console.log(myNewFunc1()); // => 1
+console.log(myNewFunc1()); // => 2
+console.log(myNewFunc2()); // => 1
+console.log(myNewFunc2()); // => 2
 
 
 // CHALLENGE 15
